f9670baf7344fb162cef7ef553aafa4350ec33c0
mustafizur-r <mustafizur.cd@gmail.com>
2026-04-23 05:04:42 +0900

    Added rokoko plugin issue

---
+++ prepare/download_rokoko_plugin.sh
@@ -0,0 +1,11 @@
+rm -rf addons
+mkdir addons
+cd addons
+
+echo -e "Downloading All necessary file"
+gdown --fuzzy https://drive.google.com/file/d/1KpjuZ2NE6YhWfsKTP_WeDwYZoCNfNyME/view?usp=sharing
+
+
+cd ../
+
+echo -e "Downloading done!"
